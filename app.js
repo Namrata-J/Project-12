f413a10ec27d14a15fd67b907a40b674578c8ebb
Namrata-J <namratajain29001@gmail.com>
2022-09-22 08:44:49 +0530

    fix: fixed the percentage logic

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ function calculatingProfitAndLoss(initial, qty, current){
         if(initial > 0 && qty > 0 && current > 0 ){
    if(initial > current){
        var loss = (initial - current) * qty;
-       var lossPercentage = (loss/initial) * 100;
+       var lossPercentage = (loss/initial);
        lossPercentage = lossPercentage.toFixed(2);
 
        showTheMessage(`Hey! the loss is ${loss} and the loss percentage is ${lossPercentage}%`, 'rgb(197, 82, 82)');
    }else{
        if(current > initial){
              var profit = (current - initial) * qty;
-             var profitPercentage = (profit/initial) * 100;
+             var profitPercentage = (profit/initial);
              profitPercentage = profitPercentage.toFixed(2);
 
              showTheMessage(`Hey! the profit is ${profit} and the profit percentage is ${profitPercentage}%`, 'rgb(147, 197, 82)');
@@ -42,7 +42,7 @@ function clickHandler(){
 
     calculatingProfitAndLoss(ip, qt, cp);
     }else{
-        showTheMessage("All the fields are mandatory to fill", 'red');
+        showTheMessage("All the fields are mandatory to fill", 'white');
     }
 }
 
